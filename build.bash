b604a1d6e96d4497bdd4aca8bbce777376ef3133
michitaro <michitaro.nike@gmail.com>
2024-11-14 12:48:13 +0900

    cleanup build script

--- build.bash
+++ build.bash
@@ -1,10 +1,9 @@
-set -e
+set -uex
 cd $(dirname $0)
-make -C /src/typescript-typevalidator
-make -C /src/stellar-globe node_modules
-make -C /src/react-stellar-globe
-make -C /src/app node_modules
-make -C /src/app -j
-make -C /src/python-integration/python setup datamodel build base_python=python3
-make -C /src/python-integration/jupyterlab-extension setup build base_python=python3
-make -C /src/python-integration/jupyterlite setup build base_python=python3
+make -C stellar-globe
+make -C react-stellar-globe
+make -C react-draggable-dialog
+make -C app -j
+make -C python-integration/python setup datamodel build base_python=python3
+make -C python-integration/jupyterlab-extension setup build base_python=python3
+make -C python-integration/jupyterlite setup build base_python=python3
