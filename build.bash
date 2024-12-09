694009d4907f202084d4decda8fbf8025c8f584c
michitaro <michitaro.nike@gmail.com>
2024-12-09 15:20:22 +0900

    cleanup

--- build.bash
+++ build.bash
@@ -3,6 +3,7 @@ cd $(dirname $0)
 make -C stellar-globe
 make -C react-stellar-globe
 make -C react-draggable-dialog
+make -C app node_modules
 make -C app -j
 make -C python-integration/python setup datamodel build base_python=python3
 make -C python-integration/jupyterlab-extension setup build base_python=python3
